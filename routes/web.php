db93cd4cd4aacf0a8227982f5881f776c789a4db
nicoysf123 <nicoyusufsulistyo234@gmail.com>
2024-05-14 18:51:11 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,9 @@ Route::get('/', function () {
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 
-<<<<<<< HEAD
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 
 Route::get('/landing-page', function () {
     $title = 'Landing Page';
     return view('pages.LandingPageUser', compact('title'));
 });
-=======
-Route::get('/register-user', [AuthController::class, 'register'])->name('register');
-
-Route::get('/register-company', [AuthController::class, 'registerCompany'])->name('registerCompany');
-Route::post('company-registration', [CompanyAuthController::class, 'register'])->name('company-registration');
->>>>>>> cf5f21deca1953605a2fc56a61212bc275152761
